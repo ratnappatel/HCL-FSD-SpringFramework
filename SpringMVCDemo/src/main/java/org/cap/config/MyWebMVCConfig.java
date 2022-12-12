4f5e89d6f4a086bf525deb3b75f9ba0a12dc523c
ratnappatel <ratnappatel@gmail.com>
2022-12-12 15:50:28 +0530

    Spring Hello World Example

---
+++ SpringMVCDemo/src/main/java/org/cap/config/MyWebMVCConfig.java
@@ -0,0 +1,24 @@
+package org.cap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//Spring -configuration
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.cap")
+public class MyWebMVCConfig {
+
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+		// helloPage ==> /WEB-INF/pages/helloPage.jsp
+		return viewResolver;
+	}
+	
+}
---
+++ SpringMVCDemo/src/main/java/org/cap/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.cap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	
+	@RequestMapping("/hello")
+	public ModelAndView sayHello() {
+		//Model = name_obj=value [MAP]
+		return new ModelAndView("helloPage","message","Hello! Welcome!"); 
+	}
+}
